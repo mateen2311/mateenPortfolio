343441eaa222522a4ccfa85bf129f751bdc2ec9e
unknown <mateen2311a@aptechgdn.net>
2024-06-05 02:46:10 +0500

    portfolio complete

---
+++ app.js
@@ -0,0 +1,8 @@
+let ham = document.querySelector('.hamburger');
+
+function displayHamb(){
+  document.querySelector('.res-nav-items').style.display = "block"
+}
+function undisplay(){
+  document.querySelector('.res-nav-items').style.display = "none"
+}
